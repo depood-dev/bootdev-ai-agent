d6a7d77d0826a826850bd0082ccf73f4f4946e72
depood <depood.dev@tuta.com>
2025-08-03 19:26:52 +0200

    update - fixed some typos

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 
@@ -16,14 +17,22 @@ def main():
         print("Error - no promt")
         sys.exit(1)
     
-    question = sys.argv[1]
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=question
+        model='gemini-2.0-flash-001', contents=messages,
     )
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "--verbose":
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
