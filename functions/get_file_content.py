4cb8bf1eb15c587e2f33662717218089bab2e8ff
depood <depood.dev@tuta.com>
2025-08-07 20:18:43 +0200

    added a TON of functionality

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,82 @@
 import os
+from google.genai import types
 from functions.config import file_char_limit
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Reads a file in the specified diectory, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                    type=types.Type.STRING,
+                    description="The path to a file to read, relative to the working directory. If not provided, lists files in the working directory itself."
+                )            
+            }
+    )
+)
+
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs a python file in the specified directory, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to a file to execute, relative to the working directory."
+            ),
+            "arguments": types.Schema(
+                type=types.Type.STRING,
+                description="Optional argments to execute the file with"
+            )
+            
+        }
+    )
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes a file in the specified directory, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to a file to write, relative to the working directory. If not provided, lists files in the working directory itself."
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write into the file"
+            )
+
+        }
+    )
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
+        ]
+    )
 
 def get_file_content(working_directory, file_path):
     try:
--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.config import system_prompt
+from functions.get_file_content import *
+from functions.get_files_info import *
+from functions.run_python_file import *
+from functions.write_file import *
 
 def main():
     # Load environment variables from a .env file so we can access sensitive keys
@@ -39,15 +44,76 @@ def main():
     generate_content(client, messages, verbose)
 
 def generate_content(client, messages, verbose):
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        )
     )
+
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)        
     print("Response:")
-    print(response.text)
+
+
+    if len(response.function_calls) > 0:
+        for call in response.function_calls:            
+            function_call_result = call_function(call, verbose)
+            if (
+                not function_call_result.parts or
+                not hasattr(function_call_result.parts[0], "function_response") or
+                not hasattr(function_call_result.parts[0].function_response, "response")
+            ):
+                raise Exception("Fatal: No function response in content part.")
+
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+        else:
+                print(response.text)
+
+def call_function(call, verbose=False):
+
+    function_map = {
+        "get_file_content": get_file_content,
+        "get_files_info": get_files_info,
+        "run_python_file": run_python_file,
+        "write_file": write_file
+    }
+
+    if call.name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=call.name,
+                    response={"error": f"Unknown function: {call.name}"},
+            )
+            ],
+        )
+    
+    args = call.args.copy()          # Copy the original args
+    args["working_directory"] = "./calculator"  # Add the required key
+    func = function_map[call.name]
+    
+    if verbose:
+        print(f"Calling function: {call.name}({call.args})")
+
+    function_result = func(**args)
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=call.name,
+                response={"result": function_result},
+            )
+        ],
+    )
+
+
 
 if __name__ == "__main__":
     main()
